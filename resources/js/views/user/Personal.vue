695791877e09caba1f527d6f52952d018a06bbea
RevenkoVladislav <Vlad_reven@mail.ru>
2025-12-14 21:40:52 +0600

    Страница Personal и форма для добавления постов

--- resources/js/views/user/Personal.vue
+++ resources/js/views/user/Personal.vue
@@ -1,11 +1,41 @@
-<script setup>
+<script>
+export default {
+  name: "Personal",
 
+  data() {
+    return {
+
+    }
+  },
+}
 </script>
 
 <template>
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <h1>hello personal</h1>
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm space-y-6">
+    <div>
+      <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+      <div class="mt-2">
+        <input v-model="title" id="title" type="text" required placeholder="Title"
+               class="block w-full rounded-md bg-gray-50 px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+      </div>
     </div>
+
+    <div>
+      <label for="content" class="block text-sm/6 font-medium text-gray-900">Content</label>
+      <div class="mt-2">
+        <textarea v-model="content" id="content" rows="4"
+                  class="block w-full rounded-md bg-gray-50 px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  placeholder="Write your content"></textarea>
+      </div>
+    </div>
+
+    <div>
+      <button type="button"
+              class="flex w-full justify-center rounded-md bg-gradient-to-r from-indigo-400 via-indigo-500 to-indigo-600 hover:bg-gradient-to-br focus:outline-none px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        Publish
+      </button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- resources/js/views/user/Register.vue
+++ resources/js/views/user/Register.vue
@@ -36,7 +36,7 @@ const submit = async () => {
                 <div>
                     <label for="name" class="block text-sm/6 font-medium text-gray-900">Name</label>
                     <div class="mt-2">
-                        <input v-model="form.name" id="name" type="text" name="name" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        <input v-model="form.name" id="name" type="text" name="name" required placeholder="Name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                     <p v-if="errors.name" class="text-red-500">
                         {{ errors.name[0] }}
@@ -46,7 +46,7 @@ const submit = async () => {
                 <div>
                     <label for="email" class="block text-sm/6 font-medium text-gray-900">Email</label>
                     <div class="mt-2">
-                        <input v-model="form.email" id="email" type="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        <input v-model="form.email" id="email" type="email" name="email" required placeholder="Email" autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                     <p v-if="errors.email" class="text-red-500">
                         {{ errors.email[0] }}
@@ -56,7 +56,7 @@ const submit = async () => {
                 <div>
                     <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
                     <div class="mt-2">
-                        <input v-model="form.password" id="password" type="password" name="password" required autocomplete="password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        <input v-model="form.password" id="password" type="password" name="password" placeholder="Password" required autocomplete="password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                     <p v-if="errors.password" class="text-red-500">
                         {{ errors.password[0] }}
@@ -66,7 +66,7 @@ const submit = async () => {
                 <div>
                     <label for="password_confirmation" class="block text-sm/6 font-medium text-gray-900">Password confirmed</label>
                     <div class="mt-2">
-                        <input v-model="form.password_confirmation" id="password_confirmation" type="password" name="password_confirmation" required autocomplete="password_confirmation" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        <input v-model="form.password_confirmation" id="password_confirmation" type="password" placeholder="Password confirmation" name="password_confirmation" required autocomplete="password_confirmation" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                     <p v-if="errors.password" class="text-red-500">
                         {{ errors.password[0] }}
