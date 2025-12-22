ace118702d9b3d7f90cf3e91be127ac85895e0b7
RevenkoVladislav <Vlad_reven@mail.ru>
2025-12-22 22:16:52 +0600

    Промежуточные изменения по реализации репоста

---
+++ database/migrations/2025_12_22_122256_add_reposted_id_to_posts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreignId('reposted_id')->nullable()->constrained('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_reposted_id_foreign');
+            $table->dropColumn('reposted_id');
+        });
+    }
+};
--- resources/js/views/user/Personal.vue
+++ resources/js/views/user/Personal.vue
@@ -138,8 +138,7 @@ export default {
             <div>
                 <label for="content" class="block text-sm/6 font-medium text-gray-900">Content</label>
                 <div class="mt-2">
-        <textarea v-model="content" id="content" rows="4" @input="errors.content = null"
-                  class="block w-full rounded-md bg-gray-50 px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    <textarea v-model="content" id="content" rows="4" @input="errors.content = null" class="block w-full rounded-md bg-gray-50 px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                   placeholder="Write your content"></textarea>
                     <p v-if="errors.content" class="text-red-500">
                         {{ errors.content[0] }}
